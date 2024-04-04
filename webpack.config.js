4893581f4bf38a86766633b0373baca2af62dc05
Eliot B <brenoteliot@gmail.com>
2024-04-04 14:42:44 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // ou 'production'
-  entry: './frontend/assets/index.js',
+  entry: './FrontEnd/assets/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
